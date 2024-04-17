4eb06c6104510fe1a016c6a9116f060e46fa1ebb
VadimMaximovv <93405971+VadimMaximovv@users.noreply.github.com>
2024-04-17 16:43:18 +0300

    Output of current processes and process completion by id

--- ProcessManager/Program.cs
+++ ProcessManager/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ProcessManager
 {
     internal class Program
     {
+        // Этот метод выводит в консольное окно все имена и индефекаторы процесов, запущенных на компьютере
+        public static void PrintAllProcess()
+        {
+            // Создание массива, в который записываются все процессы
+            Process[] localAll = Process.GetProcesses();
+            // Перечисление всех элементов массива
+            for (int i = 0; i < localAll.Length; i++)
+            {
+                // Вывод Id и имени массива
+                Console.WriteLine(localAll[i].Id + "\t" + localAll[i].ProcessName);
+            }
+            Console.WriteLine("");
+        }
+
+        // Метод завершения процесса по его Id
+        public static void KillProcess(int id)
+        {
+            // Создание локальной переменной, которой будет присвоино значение процесса
+            Process process;
+            // Запуск обработки исключений
+            try
+            {
+                // Запись процесса в переменную
+                process = Process.GetProcessById(id);
+            }
+            // Если исключение найдено, то выполняется следующий блок
+            catch (ArgumentException) 
+            {
+                // Вывод сообщения о том, что процесс по данному Id не был найден
+                Console.WriteLine("The process with this id was not found");
+                // Завершение выполнения метода
+                return;
+            }
+            // Если исключение не было найдено, вывод в консоль сообщения, содержащую информацию об завершенном процессе
+            Console.WriteLine("Process " + process.Id + " " + process.ProcessName + " forcibly terminated");
+            // Завершение процесса
+            process.Kill();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
--- ProcessManager/obj/Debug/net5.0/ProcessManager.AssemblyInfo.cs
+++ ProcessManager/obj/Debug/net5.0/ProcessManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d703079ae3c4b2a9a0193b396ebb0cd060f7e6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b43474cc6f661891187d3bd9e84b90e6ae050e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
